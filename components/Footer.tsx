32ee79a722737ed7b7d0b1d3d9b1daa166ba55ac
Spratch <contact@josephclenet.fr>
2026-07-04 17:40:59 +0200

    feat(layout): footer responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,9 +7,9 @@ export default async function Footer() {
 
   if (!footer) return null;
   return (
-    <footer className="relative grid min-h-(--h-section) grid-cols-6 content-start gap-x-2.5 gap-y-17 overflow-hidden bg-ardoise px-6 py-34 font-serif text-creme">
+    <footer className="relative flex min-h-(--h-section) grid-cols-6 flex-col content-start gap-x-2.5 gap-y-10 overflow-hidden bg-ardoise px-3 pt-16 pb-44 font-serif text-creme sm:grid sm:gap-y-17 sm:px-6 sm:pt-34 sm:pb-96">
       {/* Contact */}
-      <div className="col-span-1 flex flex-col gap-10">
+      <div className="col-span-1 flex flex-col gap-6 sm:gap-10">
         <h2>Contact</h2>
 
         <div className="flex flex-col gap-5">
@@ -46,7 +46,7 @@ export default async function Footer() {
 
       {/* Sentence */}
       <p
-        className="col-span-3 text-4xl text-balance"
+        className="col-span-3 text-3xl text-balance sm:text-4xl"
         style={{
           textBox: "trim-both cap alphabetic"
         }}
@@ -56,7 +56,7 @@ export default async function Footer() {
 
       {/* Social */}
       {footer.socials && footer.socials.length > 0 && (
-        <div className="col-span-1 col-start-2 flex flex-col gap-10">
+        <div className="col-span-1 col-start-2 flex flex-col gap-6 sm:gap-10">
           <h2>Social</h2>
 
           <div className="flex flex-col text-sm">
@@ -76,7 +76,7 @@ export default async function Footer() {
 
       {/* Jobs */}
       {footer.jobs && (
-        <div className="col-span-2 col-start-3 flex flex-col gap-10">
+        <div className="col-span-2 col-start-3 flex flex-col gap-6 sm:gap-10">
           <h2>{footer.jobs.title}</h2>
 
           <p className="text-sm">{footer.jobs.text}</p>
@@ -95,7 +95,7 @@ export default async function Footer() {
       )}
 
       {/* Logo */}
-      <Logo className="footer-logo absolute bottom-[-7vw] fill-transparent stroke-creme stroke-1 px-6" />
+      <Logo className="footer-logo absolute bottom-[-7vw] mask-b-from-20% mask-b-to-80% fill-transparent stroke-creme stroke-1 pr-3 sm:px-6" />
     </footer>
   );
 }
