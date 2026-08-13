a6cdb83e3495fdf1423cedec553ea6c913a77bdf
Spratch <contact@josephclenet.fr>
2026-08-13 13:14:55 +0200

    feat(no content section): sticky text

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -19,7 +19,7 @@ export default async function Home({
       <main
         data-page-bg={home.pageColors.backgroundColor.slug}
         data-page-text={home.pageColors.textColor.slug}
-        className="flex flex-col gap-3 overflow-x-hidden bg-(--background-color) px-3 pt-(--h-header) pb-3 text-(--text-color) **:[path]:fill-(--color-logo)"
+        className="flex flex-col gap-3 bg-(--background-color) px-3 pt-(--h-header) pb-3 text-(--text-color) **:[path]:fill-(--color-logo)"
         style={
           {
             "--color-logo": home.logoColor
--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -62,7 +62,7 @@ export default async function Section({ section }: SectionProps) {
         >
           {/* Texts */}
           <div
-            className={`col-span-3 flex flex-col gap-10 ${section.description?.layout.position === "bottom" ? "h-full justify-between" : "justify-start"}`}
+            className={`col-span-3 flex flex-col gap-10 ${section.description?.layout.position === "bottom" ? "h-full justify-between" : "justify-start"} ${!section.content && "sticky top-16"} ${!section.content && section.button?.position === "bottom" ? "pb-16" : ""}`}
           >
             <div className="flex flex-col gap-5">
               {(section.subtitle || section.contentType === "experience") && (
